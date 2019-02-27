3e51dcaab29859fc40c3452310b8d5c306d47f51
JoseHuert <jhuerta060@gmail.com>
2019-02-27 09:22:34 -0600

    Creación de los proyectos, instalación de los paquetes Nuget, comienzo pantalla LogIn

--- RefaccionariaHuerta/View/Forms/LogIn/LogIn.xaml.cs
+++ RefaccionariaHuerta/View/Forms/LogIn/LogIn.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +11,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RefaccionariaHuerta
+namespace View.Forms.LogIn
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para LogIn.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LogIn : MetroWindow
     {
-        public MainWindow()
+        public LogIn()
         {
             InitializeComponent();
         }
